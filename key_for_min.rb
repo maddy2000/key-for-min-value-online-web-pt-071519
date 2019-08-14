24afb81de58ac9fa8d07f48050675f26443fe15f
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-14 21:52:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  lowest_key - nil
+  lowest_key = nil
   lowest_value = nil
   hash.each do |key, value|
     if lowest_value == nil
